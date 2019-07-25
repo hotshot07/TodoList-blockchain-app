8962360881ef9b435a6391d730942d225d3dae68
hotshot07 <mayank.arora2025@gmail.com>
2019-07-26 01:31:32 +0530

    Basic Task Done

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,25 @@ pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount = 0;
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    //like a database dictionary
+    mapping(uint => Task) public tasks;
+
+    constructor() public{
+        createTask("Check out mackweb.in");
+    }
+    //created a function to make tasks
+    //it increments the task count
+    //adds
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
